1d4417c9b79d3398d8ea4b43b7e443bf094ad121
Marc Jou <smyslov@gmail.com>
2023-09-09 10:51:56 +0200

    Add simple test

--- server/clojure/simple-server/project.clj
+++ server/clojure/simple-server/project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.json "2.4.0"]
                  [compojure "1.7.0"]
                  [clj-http "3.12.3"]
                  [ring/ring-defaults "0.3.4"]
--- server/clojure/simple-server/test/simple_server/handler_test.clj
+++ server/clojure/simple-server/test/simple_server/handler_test.clj
@@ -1,13 +1,22 @@
 (ns simple-server.handler-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest is testing]]
+            [clojure.data.json :as J]
             [ring.mock.request :as mock]
-            [simple-server.handler :refer :all]))
+            [simple-server.handler :refer [app]]))
+
+
+(defn JS
+  "Helper to convert map to string"
+  [s]
+  (J/write-str s))
+
 
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
+      (println response)
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:body response) (JS {:status "Ok"})))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
